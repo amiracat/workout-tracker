10bce5e8f4bec195204ce62f582112cfa78a36f4
Amira Holden <amiraholden@gmail.com>
2021-08-12 14:36:24 -0500

    adding initial files

---
+++ models/workout.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  name: String,
+  title: String
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
