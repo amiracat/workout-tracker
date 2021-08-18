40c88b96ba59e50f3ca364ff615e9c5d1ab3165a
Amira Holden <amiraholden@gmail.com>
2021-08-18 14:11:49 -0500

    removed unnecessary code

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,30 +2,6 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 const path = require("path");
 
-//routes to send to different html pages
-
-// const dbWorkout = new Workout();
-
-// Workout.create({ name: "workouts" })
-//   .then(dbWorkout => {
-//     console.log(dbWorkout);
-//   })
-//   .catch(({message}) => {
-//     console.log(message);
-//   });
-
-// router.post("/exercise", ({body}, res) => {
-//   Workout.create(body)
-//     .then(({_id}) => Workout.findOneAndUpdate({}, { $push: { workouts: _id } }, { new: true }))
-//     .then(dbWorkout => {
-//       res.json(dbWorkout);
-//       console.log(dbWorkout);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
  });
